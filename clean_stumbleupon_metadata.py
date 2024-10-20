6b82d119b1d321d62e241ce3e3d799f7b6242c8b
Jake Lee <github@jakelee.co.uk>
2024-10-20 18:02:10 +0100

    Adds toprated & non-toprated cleans

--- clean_stumbleupon_metadata.py
+++ clean_stumbleupon_metadata.py
@@ -2,5 +2,7 @@ import pandas as pd
 import csv
 
 df = pd.read_csv('data-parsed/parsed.csv')
+# df = df[df['source'] != 'toprated'] # Remove top rated
+# df = df[df['source'] == 'toprated'] # Keep only top rated
 df = df.drop_duplicates(subset='id', keep='last')
 df.to_csv('data-parsed/parsed-cleaned.csv', index=False, quoting=csv.QUOTE_NONNUMERIC)
